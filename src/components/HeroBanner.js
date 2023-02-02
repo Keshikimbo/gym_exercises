a0fb16e7909532b296394bc780ce9a8465580cca
Ivan Onofriichuk <ivanonofrijcuk@gmail.com>
2023-02-02 22:13:52 +0200

    start work

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box, Stack, Typography} from '@mui/material'
+
+export const HeroBanner = () => {
+  return (
+	<Box>
+	<Typography color ='#FF2625'>
+		Fitness Club	
+	</Typography>	
+	</Box>
+  )
+}
